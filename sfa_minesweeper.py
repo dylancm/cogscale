f93ed371c7284bd9a1b4152a9d36cce5b0035fa1
Dylan Mankey <dylancm@gmail.com>
2015-08-02 19:07:08 -0500

    adding runner to help with output and handling points, plus an ungle workaround for a bug

--- sfa_minesweeper.py
+++ sfa_minesweeper.py
@@ -22,6 +22,10 @@ class MineSweeperSim:
   }
 
   move_counter = 0
+  mine_passed = False
+  points = 0
+  shot_penalty_counter = 0
+  move_penalty_counter = 0
 
   def print_current_field(self):
     # for readabilities sake I'll define the following variables so that tuple indexes are more
@@ -53,6 +57,13 @@ class MineSweeperSim:
     # mines.
     mine_x_offset = self.sweeper_location[-1][x] - int(((max_x_diff * 2) + 1)/2)
     mine_y_offset = self.sweeper_location[-1][y] - int(((max_y_diff * 2) + 1)/2)
+
+    # This is ugly, but there's a corner case where the offsets can get our of range
+    if mine_x_offset > max_x_diff:
+      mine_x_offset *= -1
+    if mine_y_offset > max_y_diff:
+      mine_y_offset *= -1
+
     # loop over our mines and insert them into the current field
     for mine, mine_z in self.mines_z_map.items():
       current_field[mine[y] + mine_y_offset][mine[x] + mine_x_offset] = mine_z
@@ -86,6 +97,12 @@ class MineSweeperSim:
         else:
           raise Exception('Invalid cuboid definition')
 
+    # initialize our points system
+    mine_count = len(self.mines_z_map.keys())
+    self.points = 10 * mine_count
+    self.shot_penalty_counter = 5 * mine_count
+    self.move_penalty_counter = 3 * mine_count
+
     # x and y max give us the lower right hand corner of the defined cuboid from there we can get
     # the center point as the starting location of the mine sweeper.
     self.sweeper_location.append((int(x_max/2), int(y_max/2), 0))
@@ -97,10 +114,6 @@ class MineSweeperSim:
     y = 1
     z = 2
 
-    print 'Step {0}'.format(self.move_counter + 1)
-    self.print_current_field()
-    print move.strip()
-
     # parse the step and see if it is a valid movement for firing pattern
     steps = move.strip().split(' ')
     for step in steps:
@@ -109,6 +122,11 @@ class MineSweeperSim:
         cur_loc = self.sweeper_location[-1]
         move = self.moves[step]
         self.sweeper_location.append((cur_loc[x] + move[x], cur_loc[y] + move[y], cur_loc[z]))
+
+        # points accounting
+        if self.move_penalty_counter:
+          self.points -= 2
+          self.move_penalty_counter -= 1
       elif step in self.firing.keys():
         pattern = self.firing[step]
         cur_loc = self.sweeper_location[-1]
@@ -116,13 +134,48 @@ class MineSweeperSim:
           torp_loc = (cur_loc[x] + off_set[x], cur_loc[y] + off_set[y])
           if torp_loc in self.mines_z_map.keys():
             del self.mines_z_map[torp_loc]
-    self.increment_move()
-    self.print_current_field()
+
+        # points accounting
+        if self.shot_penalty_counter:
+          self.points -= 5
+          self.shot_penalty_counter -= 1
 
   def increment_move(self):
     self.move_counter += 1
     for mine, z in self.mines_z_map.items():
+      if z + 1 >= 0:
+        self.mine_passed = True
       self.mines_z_map[mine] = z + 1
+
+  def run_sim(self, moves):
+    """ Handles running the sim from the input and providing output
+    """
+
+    for move in moves:
+      # check if we've all mines are gone
+      if len(self.mines_z_map.keys()) == 0:
+        self.points = 1
+
+      print 'Step {0}'.format(self.move_counter + 1)
+      print
+      self.print_current_field()
+      print
+      print move.strip()
+      print
+      self.execute_move(move)
+      self.increment_move()
+      self.print_current_field()
+      print
+      if self.mine_passed:
+        # if we pass a mine we fail with 0 points
+        print 'fail (0)'
+        break
+
+    # if we've done all the moves form the script and there are still mines we fail with - points
+    if len(self.mines_z_map.keys()) != 0:
+      print 'fail (0)'
+
+    print 'pass ({0})'.format(self.points)
 
 
 if __name__ == '__main__':
@@ -131,6 +184,4 @@ if __name__ == '__main__':
 
   sim = MineSweeperSim()
   sim.parse_cuboid(cuboid_if.readlines())
-
-  for move in moves_if.readlines():
-    sim.execute_move(move)
+  sim.run_sim(moves_if.readlines())
